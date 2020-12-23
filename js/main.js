f6102a95c77ec148f37fd1f8d4dbba84b12c149b
Raha <erbees.50000@gmail.com>
2020-12-23 14:29:36 +0700

    Deploy

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+var typed = new Typed('#typed', {
+      strings:['My name is Rahadina Budiman Sundara',
+               'I live in Indonesia',
+               'Reading, Listen Music, and wrote some code was my Hobby',
+               'Check on buttons below if you want to visit my social media.',
+               'Cheers!',
+            ],
+      typeSpeed: 45,
+      backSpeed: 80,
+      fadeOut: true,
+      loop: true
+    });
+
+});
