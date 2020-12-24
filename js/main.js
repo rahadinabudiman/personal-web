a9adf9c6c4c8ed5a0612bd3ffabf87af979729ad
Raha <erbees.50000@gmail.com>
2020-12-24 13:06:13 +0700

    some updates

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
 var typed = new Typed('#typed', {
-      strings:['My name is Rahadina Budiman Sundara',
-               'I live in Indonesia',
-               'Reading, Listen Music, and wrote some code was my Hobby',
+      strings:['My name is Rahadina Budiman Sundara.',
+               'I live in Indonesia.',
+               'Reading, Listen Music, and wrote some code was my Hobby.',
                'Check on buttons below if you want to visit my social media.',
                'Cheers!',
             ],
